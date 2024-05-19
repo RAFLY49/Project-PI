567287dce30a6f4057fee0532ad05f08ba7c542d
RAFLY49 <117022584+RAFLY49@users.noreply.github.com>
2024-05-19 11:34:17 +0700

    gemini_utility.py

--- streamlit/gemini_utility.py
+++ streamlit/gemini_utility.py
@@ -1,18 +1,27 @@
 import os
 import json
 from PIL import Image
-
 import google.generativeai as genai
 
 # working directory path
 working_dir = os.path.dirname(os.path.abspath(__file__))
 
 # path of config_data file
-config_file_path = f"{working_dir}/config.json"
-config_data = json.load(open("config.json"))
+config_file_path = os.path.join(working_dir, 'config.json')
+
+# Ensure the config file exists
+if not os.path.exists(config_file_path):
+    raise FileNotFoundError(f"Config file not found: {config_file_path}")
+
+# Load config data
+with open(config_file_path, 'r') as config_file:
+    config_data = json.load(config_file)
 
 # loading the GOOGLE_API_KEY
-GOOGLE_API_KEY = config_data["GOOGLE_API_KEY"]
+GOOGLE_API_KEY = config_data.get("GOOGLE_API_KEY")
+
+if not GOOGLE_API_KEY:
+    raise ValueError("GOOGLE_API_KEY not found in the config file")
 
 # configuring google.generativeai with API key
 genai.configure(api_key=GOOGLE_API_KEY)
